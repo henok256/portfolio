6872164d3b20f222275b02a54d29291162a041fb
henok256 <henokmengesha91@gmail.com>
2021-10-04 02:01:38 -0400

    commit done after correcting the href of img tag

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -13,13 +13,13 @@ export default function Works() {
             <div className="left_container">
                 <img src="assets/webapp.png" width="500px" height="500px" alt="" />
                 <h3>E-Commerece Web App</h3>
-                <p ><a href="https://www.github.com/henok256" target="_blank">Github repository</a></p>
+                <p ><a href="https://github.com/henok256/Ecommerce-app" target="_blank" rel="noreferrer">Github repository</a></p>
             </div>
 
             <div className="right_container">
-                <img src="https://cdn.dribbble.com/users/1726280/screenshots/13145817/media/98b3a7f831cc22126c69b74c230459ab.jpg?compress=1&resize=400x300" width="500px" height="500px" />
+                <img src="https://cdn.dribbble.com/users/1726280/screenshots/13145817/media/98b3a7f831cc22126c69b74c230459ab.jpg?compress=1&resize=400x300" alt="pictur for mobile app" width="500px" height="500px" />
                 <h3 >Mobile App</h3>
-                <p ><a href="https://www.github.com/henok256" target="_blank">Github repository</a></p>
+                <p ><a href="https://github.com/Ibro365/Geeazy_Mobile_App" target="_blank" rel="noreferrer">Github repository</a></p>
 
             </div>
 
